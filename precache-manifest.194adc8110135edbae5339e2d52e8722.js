ee63bc99c22d63665e02a6c2c0f473344c33b665
Michal Matwiejczuk <michal.matwiejczuk@lingarogroup.com>
2020-07-21 15:59:37 +0200

    uploaded build

---
+++ precache-manifest.194adc8110135edbae5339e2d52e8722.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f783e366008fc4b3936034b219ce120e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8fafecaa2dd22b33525",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "dfb206994f58db750d4b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b8fafecaa2dd22b33525",
+    "url": "/static/js/2.90f6525f.chunk.js"
+  },
+  {
+    "revision": "df62e17367f7fd5819a3e9b999282991",
+    "url": "/static/js/2.90f6525f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfb206994f58db750d4b",
+    "url": "/static/js/main.bff1ab01.chunk.js"
+  },
+  {
+    "revision": "3f9b3c44b97da5fd9d62",
+    "url": "/static/js/runtime-main.7497fed8.js"
+  }
+]);
